e8968809037922ed92ea7a4db21150a738e3311d
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 20:01:01 +0900

    GlassLighting.swift 업데이트

--- DesignSystem/Glass/GlassLighting.swift
+++ DesignSystem/Glass/GlassLighting.swift
@@ -1,1 +1,28 @@
+import SwiftUI
 
+struct GlassLighting: View {
+
+    var body: some View {
+
+        LinearGradient(
+
+            colors: [
+
+                .white.opacity(0.42),
+
+                .white.opacity(0.08),
+
+                .clear
+
+            ],
+
+            startPoint: .topLeading,
+
+            endPoint: .bottomTrailing
+
+        )
+        .blendMode(.screen)
+
+    }
+
+}
