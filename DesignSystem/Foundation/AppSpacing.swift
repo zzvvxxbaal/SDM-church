46c85baf935405616e4d77b41119b2f65b2d4693
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 19:44:06 +0900

    AppSpacing.swift 업데이트

--- DesignSystem/Foundation/AppSpacing.swift
+++ DesignSystem/Foundation/AppSpacing.swift
@@ -1,1 +1,12 @@
+import CoreGraphics
 
+enum AppSpacing {
+
+    static let xSmall: CGFloat = 4
+    static let small: CGFloat = 8
+    static let medium: CGFloat = 16
+    static let large: CGFloat = 24
+    static let xLarge: CGFloat = 32
+    static let xxLarge: CGFloat = 48
+
+}
