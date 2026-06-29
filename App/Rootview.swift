b7201a7018d1a6d9bbe35801a822fec2287e3bff
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 19:50:05 +0900

    Rootview.swift 업데이트

--- App/Rootview.swift
+++ App/Rootview.swift
@@ -1,1 +1,25 @@
+import SwiftUI
 
+struct RootView: View {
+
+    var body: some View {
+
+        NavigationStack {
+
+            LoginView()
+
+        }
+
+        .preferredColorScheme(.light)
+
+        .tint(AppColors.tint)
+
+    }
+
+}
+
+#Preview {
+
+    RootView()
+
+}
