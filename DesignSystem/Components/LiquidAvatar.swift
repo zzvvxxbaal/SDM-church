ed38bb059faf847b41aa423f8f94e37a5e1b1d99
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 19:55:35 +0900

    LiquidAvatar.swift 업데이트

--- DesignSystem/Components/LiquidAvatar.swift
+++ DesignSystem/Components/LiquidAvatar.swift
@@ -1,1 +1,57 @@
+import SwiftUI
 
+struct LiquidAvatar: View {
+
+    var image: Image?
+
+    var initials: String = "S"
+
+    var size: CGFloat = 72
+
+    var body: some View {
+
+        ZStack {
+
+            Circle()
+
+                .fill(.ultraThinMaterial)
+
+            if let image {
+
+                image
+
+                    .resizable()
+
+                    .scaledToFill()
+
+            } else {
+
+                Text(initials)
+
+                    .font(.system(size: size * 0.36, weight: .bold))
+
+            }
+
+        }
+
+        .frame(width: size, height: size)
+
+        .clipShape(Circle())
+
+        .overlay {
+
+            Circle()
+
+                .stroke(.white.opacity(0.35), lineWidth: 1)
+
+        }
+
+    }
+
+}
+
+#Preview {
+
+    LiquidAvatar()
+
+}
