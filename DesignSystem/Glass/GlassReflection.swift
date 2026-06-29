7801bf91dbcf68642bb098f401b60f41b1216791
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 19:47:41 +0900

    GlassReflection.swift 업데이트

--- DesignSystem/Glass/GlassReflection.swift
+++ DesignSystem/Glass/GlassReflection.swift
@@ -1,1 +1,29 @@
+import SwiftUI
 
+struct GlassReflection: View {
+
+    var body: some View {
+
+        LinearGradient(
+
+            colors: [
+
+                .clear,
+
+                .white.opacity(0.20),
+
+                .clear
+
+            ],
+
+            startPoint: .leading,
+
+            endPoint: .trailing
+
+        )
+
+        .rotationEffect(.degrees(18))
+
+    }
+
+}
