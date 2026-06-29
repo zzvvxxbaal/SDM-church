91ee53047a70ab34ecabdea32cf994198f49991b
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 19:44:24 +0900

    AppRadius.swift 업데이트

--- DesignSystem/Foundation/AppRadius.swift
+++ DesignSystem/Foundation/AppRadius.swift
@@ -1,1 +1,11 @@
+import CoreGraphics
 
+enum AppRadius {
+
+    static let button: CGFloat = 30
+    static let card: CGFloat = 34
+    static let sheet: CGFloat = 40
+    static let dialog: CGFloat = 36
+    static let avatar: CGFloat = 999
+
+}
