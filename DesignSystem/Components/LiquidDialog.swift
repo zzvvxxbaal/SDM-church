04fd0f8f3c55ab96d24db58944c97050c9e8c469
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 20:42:58 +0900

    LiquidDialog.swift 업데이트

--- DesignSystem/Components/LiquidDialog.swift
+++ DesignSystem/Components/LiquidDialog.swift
@@ -1,1 +1,23 @@
+import SwiftUI
 
+struct LiquidDialog<Content: View>: View {
+
+    @ViewBuilder
+
+    let content: Content
+
+    var body: some View {
+
+        VStack(spacing:20) {
+
+            content
+
+        }
+
+        .padding(28)
+
+        .glass()
+
+    }
+
+}
