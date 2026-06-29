a91f347b92401b8db943fb455138a7f453265db9
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 20:39:39 +0900

    GlassMorph.swift 업데이트

--- DesignSystem/Glass/GlassMorph.swift
+++ DesignSystem/Glass/GlassMorph.swift
@@ -1,1 +1,45 @@
+import SwiftUI
 
+struct GlassMorph: ViewModifier {
+
+    var pressed: Bool
+
+    func body(content: Content) -> some View {
+
+        content
+
+            .scaleEffect(
+                pressed ? 0.965 : 1
+            )
+
+            .rotation3DEffect(
+
+                .degrees(
+
+                    pressed ? 0.8 : 0
+
+                ),
+
+                axis: (
+
+                    x: 1,
+
+                    y: 0,
+
+                    z: 0
+
+                )
+
+            )
+
+            .animation(
+
+                Springs.button,
+
+                value: pressed
+
+            )
+
+    }
+
+}
