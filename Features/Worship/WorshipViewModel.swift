4c20fd2211e63f9e32a7de9aa74bf1faef00db9d
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 20:46:50 +0900

    WorshipViewModel.swift 업데이트

--- Features/Worship/WorshipViewModel.swift
+++ Features/Worship/WorshipViewModel.swift
@@ -1,1 +1,13 @@
+import Foundation
+import Observation
 
+@Observable
+final class WorshipViewModel {
+
+    let title = "예배"
+
+    let nextWorship = "주일 오후 2:00"
+
+    let location = "서대문교회 청년부 예배실"
+
+}
