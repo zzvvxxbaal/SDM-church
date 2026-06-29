99b9fc4750d28b609d2318145dcd797d393602fe
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 20:55:12 +0900

    FloatingGlassTapBar.swift 업데이트

--- DesignSystem/Components/FloatingGlassTapBar.swift
+++ DesignSystem/Components/FloatingGlassTapBar.swift
@@ -1,1 +1,35 @@
+import SwiftUI
 
+struct FloatingGlassTabBar<Content: View>: View {
+
+    @ViewBuilder
+
+    let content: Content
+
+    var body: some View {
+
+        HStack {
+
+            content
+
+        }
+
+        .padding(.horizontal,24)
+
+        .padding(.vertical,14)
+
+        .glass()
+
+        .shadow(
+
+            color: .black.opacity(0.12),
+
+            radius: 35,
+
+            y: 18
+
+        )
+
+    }
+
+}
