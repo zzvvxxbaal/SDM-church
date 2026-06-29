d1febe744fb2b85c49789d9ef807c8ed37bc2d0d
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 20:51:37 +0900

    GlassEnvironmentReflection.swift 업데이트

--- DesignSystem/Glass/GlassEnvironmentReflection.swift
+++ DesignSystem/Glass/GlassEnvironmentReflection.swift
@@ -1,1 +1,37 @@
+import SwiftUI
 
+struct GlassEnvironmentReflection: View {
+
+    var x: CGFloat
+
+    var y: CGFloat
+
+    var body: some View {
+
+        RadialGradient(
+
+            colors: [
+
+                .white.opacity(0.30),
+
+                .white.opacity(0.08),
+
+                .clear
+
+            ],
+
+            center: .center,
+
+            startRadius: 0,
+
+            endRadius: 260
+
+        )
+
+        .offset(x: x, y: y)
+
+        .blendMode(.screen)
+
+    }
+
+}
