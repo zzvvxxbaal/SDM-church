9e92f37c7bac059f13f1769ac7e32208db94b4eb
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 19:45:13 +0900

    Springs.swift 업데이트

--- DesignSystem/Animation/Springs.swift
+++ DesignSystem/Animation/Springs.swift
@@ -1,1 +1,20 @@
+import SwiftUI
 
+enum Springs {
+
+    static let button = Animation.spring(
+        duration: 0.45,
+        bounce: 0.25
+    )
+
+    static let card = Animation.spring(
+        duration: 0.55,
+        bounce: 0.18
+    )
+
+    static let sheet = Animation.spring(
+        duration: 0.60,
+        bounce: 0.22
+    )
+
+}
