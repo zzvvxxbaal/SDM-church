877f3230900c40fcd98297f070f87aff2e9bdd55
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 20:47:18 +0900

    PrayerViewModel.swift 업데이트

--- Features/Prayer/PrayerViewModel.swift
+++ Features/Prayer/PrayerViewModel.swift
@@ -1,1 +1,19 @@
+import Foundation
+import Observation
 
+@Observable
+final class PrayerViewModel {
+
+    var prayers: [String] = [
+
+        "청년부 부흥을 위해",
+
+        "새가족 정착을 위해",
+
+        "다음 세대를 위해",
+
+        "선교를 위해"
+
+    ]
+
+}
