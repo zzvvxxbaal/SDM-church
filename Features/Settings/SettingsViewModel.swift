7955f47f907262b5c6fe8e23e0d4145a09b96ee0
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 20:49:01 +0900

    SettingsViewModel.swift 업데이트

--- Features/Settings/SettingsViewModel.swift
+++ Features/Settings/SettingsViewModel.swift
@@ -1,1 +1,21 @@
+import Foundation
+import Observation
 
+@Observable
+final class SettingsViewModel {
+
+    let items = [
+
+        "알림",
+
+        "다크모드",
+
+        "개인정보",
+
+        "문의하기",
+
+        "앱 정보"
+
+    ]
+
+}
