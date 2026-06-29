73090b20bd3134670a98ff89f72d45de115c8a56
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 20:47:55 +0900

    CommunityViewModel.swift 업데이트

--- Features/Community/CommunityViewModel.swift
+++ Features/Community/CommunityViewModel.swift
@@ -1,1 +1,19 @@
+import Foundation
+import Observation
 
+@Observable
+final class CommunityViewModel {
+
+    let groups = [
+
+        "1조",
+
+        "2조",
+
+        "3조",
+
+        "4조"
+
+    ]
+
+}
