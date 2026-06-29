f1556cb17be340842b4a60ab683d4a6ca73025ea
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 20:42:42 +0900

    LiquidBottomSheet.swift 업데이트

--- DesignSystem/Components/LiquidBottomSheet.swift
+++ DesignSystem/Components/LiquidBottomSheet.swift
@@ -1,1 +1,31 @@
+import SwiftUI
 
+struct LiquidBottomSheet<Content: View>: View {
+
+    @ViewBuilder
+
+    let content: Content
+
+    var body: some View {
+
+        VStack {
+
+            Capsule()
+
+                .fill(.secondary.opacity(0.4))
+
+                .frame(width:44,height:5)
+
+                .padding(.top,12)
+
+            content
+
+        }
+
+        .padding(24)
+
+        .glass()
+
+    }
+
+}
