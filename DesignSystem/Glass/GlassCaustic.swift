e1d4cd8472f4cc122097948360f786b248f4b1b1
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 20:53:14 +0900

    GlassCaustic.swift 업데이트

--- DesignSystem/Glass/GlassCaustic.swift
+++ DesignSystem/Glass/GlassCaustic.swift
@@ -1,1 +1,29 @@
+import SwiftUI
 
+struct GlassCaustic: View {
+
+    var body: some View {
+
+        AngularGradient(
+
+            colors: [
+
+                .white.opacity(0.08),
+
+                .clear,
+
+                .white.opacity(0.05),
+
+                .clear
+
+            ],
+
+            center: .center
+
+        )
+
+        .blur(radius: 40)
+
+    }
+
+}
