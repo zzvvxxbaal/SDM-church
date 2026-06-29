4f39bda8e8ecba2eafc7be11f99a281e169e4036
zzvvxxbaal <w1tns61@gmail.com>
2026-06-29 20:52:30 +0900

    GlassSpecular.swift 업데이트

--- DesignSystem/Glass/GlassSpecular.swift
+++ DesignSystem/Glass/GlassSpecular.swift
@@ -1,1 +1,29 @@
+import SwiftUI
 
+struct GlassSpecular: View {
+
+    var body: some View {
+
+        LinearGradient(
+
+            colors: [
+
+                .white.opacity(0.55),
+
+                .white.opacity(0.18),
+
+                .clear
+
+            ],
+
+            startPoint: .topLeading,
+
+            endPoint: .bottomTrailing
+
+        )
+
+        .blendMode(.plusLighter)
+
+    }
+
+}
