f134295b947e626a58ca944c9c17ac7ad580483d
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 18:51:52 +0900

    AppRouter.swift 업데이트

--- Shared/Services/AppRouter.swift
+++ Shared/Services/AppRouter.swift
@@ -1,1 +1,18 @@
+import Observation
 
+@Observable
+final class AppRouter {
+
+    enum Tab {
+
+        case home
+        case worship
+        case prayer
+        case community
+        case settings
+
+    }
+
+    var selectedTab: Tab = .home
+
+}
