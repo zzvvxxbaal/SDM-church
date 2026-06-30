eb033c8041ead3accf967d3821224c17089305b4
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 18:22:00 +0900

    LiquidBadge.swift 업데이트

--- DesignSystem/Components/LiquidBadge.swift
+++ DesignSystem/Components/LiquidBadge.swift
@@ -1,1 +1,21 @@
+import SwiftUI
 
+struct LiquidBadge: View {
+
+    let title: String
+
+    var body: some View {
+
+        Text(title)
+
+            .font(.caption.weight(.semibold))
+
+            .padding(.horizontal,12)
+
+            .padding(.vertical,7)
+
+            .glass()
+
+    }
+
+}
