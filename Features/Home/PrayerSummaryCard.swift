51207c68d725dc6d84fcfd21c4d8457b168de3a4
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 19:18:08 +0900

    PrayerSummaryCard.swift 업데이트

--- Features/Home/PrayerSummaryCard.swift
+++ Features/Home/PrayerSummaryCard.swift
@@ -1,1 +1,37 @@
+import SwiftUI
 
+struct PrayerSummaryCard: View {
+
+    var body: some View {
+
+        LiquidCard {
+
+            HStack {
+
+                VStack(alignment: .leading, spacing: 10) {
+
+                    Text("기도제목")
+
+                        .font(.headline)
+
+                    Text("3개의 새로운 기도제목")
+
+                        .foregroundStyle(.secondary)
+
+                }
+
+                Spacer()
+
+                Image(systemName: "hands.sparkles.fill")
+
+                    .font(.system(size: 36))
+
+                    .foregroundStyle(.orange)
+
+            }
+
+        }
+
+    }
+
+}
