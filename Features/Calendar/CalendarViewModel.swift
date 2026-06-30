04f1846784330180128bbf3c455f878bc30d86a1
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 18:34:36 +0900

    CalendarViewModel.swift 업데이트

--- Features/Calendar/CalendarViewModel.swift
+++ Features/Calendar/CalendarViewModel.swift
@@ -1,1 +1,42 @@
+import Foundation
+import Observation
 
+@Observable
+
+final class CalendarViewModel {
+
+    let events = [
+
+        CalendarEvent(
+
+            title: "주일예배",
+
+            time: "14:00",
+
+            place: "청년부실"
+
+        ),
+
+        CalendarEvent(
+
+            title: "금요기도회",
+
+            time: "20:00",
+
+            place: "본당"
+
+        ),
+
+        CalendarEvent(
+
+            title: "소그룹",
+
+            time: "19:00",
+
+            place: "각 조 모임"
+
+        )
+
+    ]
+
+}
