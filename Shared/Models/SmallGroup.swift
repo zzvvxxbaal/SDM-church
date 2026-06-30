02a5eb8527e5310a03c2b188066f66514845c440
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 18:43:05 +0900

    SmallGroup.swift 업데이트

--- Shared/Models/SmallGroup.swift
+++ Shared/Models/SmallGroup.swift
@@ -1,1 +1,15 @@
+import Foundation
 
+struct SmallGroup: Identifiable {
+
+    let id = UUID()
+
+    let leader: String
+
+    let groupName: String
+
+    let location: String
+
+    let memberCount: Int
+
+}
