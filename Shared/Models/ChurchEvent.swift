e3a47dc750c57ee0eb023e21d2552d4ee4078523
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 19:26:21 +0900

    ChurchEvent.swift 업데이트

--- Shared/Models/ChurchEvent.swift
+++ Shared/Models/ChurchEvent.swift
@@ -1,1 +1,19 @@
+import Foundation
 
+struct ChurchEvent: Identifiable, Hashable {
+
+    let id = UUID()
+
+    let title: String
+
+    let startDate: Date
+
+    let endDate: Date
+
+    let location: String
+
+    let description: String
+
+    let imageName: String
+
+}
