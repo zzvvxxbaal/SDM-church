eaead85e5be8db8a7bef590a63986d17afa2d2bb
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 18:21:16 +0900

    GlassDivider.swift 업데이트

--- DesignSystem/Components/GlassDivider.swift
+++ DesignSystem/Components/GlassDivider.swift
@@ -1,1 +1,19 @@
+import SwiftUI
 
+struct GlassDivider: View {
+
+    var body: some View {
+
+        Rectangle()
+
+            .fill(
+
+                .white.opacity(0.12)
+
+            )
+
+            .frame(height: 1)
+
+    }
+
+}
