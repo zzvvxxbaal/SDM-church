b633c1318bd1989f658f497a922b70421573cc71
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 18:53:05 +0900

    HapticManager.swift 업데이트

--- Shared/Managers/HapticManager.swift
+++ Shared/Managers/HapticManager.swift
@@ -1,1 +1,29 @@
+import UIKit
 
+enum HapticManager {
+
+    static func light() {
+
+        UIImpactFeedbackGenerator(style: .light)
+
+            .impactOccurred()
+
+    }
+
+    static func medium() {
+
+        UIImpactFeedbackGenerator(style: .medium)
+
+            .impactOccurred()
+
+    }
+
+    static func heavy() {
+
+        UIImpactFeedbackGenerator(style: .heavy)
+
+            .impactOccurred()
+
+    }
+
+}
