c7bff66c26ba364324e5619bf2faf835b54cad49
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 19:28:59 +0900

    WorshipCountdownCard.swift 업데이트

--- Features/Home/WorshipCountdownCard.swift
+++ Features/Home/WorshipCountdownCard.swift
@@ -1,1 +1,43 @@
+import SwiftUI
 
+struct WorshipCountdownCard: View {
+
+    var body: some View {
+
+        LiquidCard {
+
+            VStack(spacing: 16) {
+
+                Text("다음 예배까지")
+
+                    .font(.headline)
+
+                Text("02일 18시간")
+
+                    .font(
+
+                        .system(
+
+                            size: 36,
+
+                            weight: .bold,
+
+                            design: .rounded
+
+                        )
+
+                    )
+
+                LiquidProgress(
+
+                    value: 0.72
+
+                )
+
+            }
+
+        }
+
+    }
+
+}
