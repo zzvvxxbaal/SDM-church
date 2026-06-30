080d840c1322d8075a21799b8303a4e211ab4736
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 18:38:23 +0900

    FloatingActionButton.swift 업데이트

--- DesignSystem/Components/FloatingActionButton.swift
+++ DesignSystem/Components/FloatingActionButton.swift
@@ -1,1 +1,41 @@
+import SwiftUI
 
+struct FloatingActionButton: View {
+
+    let systemImage: String
+
+    let action: () -> Void
+
+    var body: some View {
+
+        Button(
+
+            action: action
+
+        ) {
+
+            Image(
+
+                systemName: systemImage
+
+            )
+
+            .font(.title2)
+
+            .frame(
+
+                width: 64,
+
+                height: 64
+
+            )
+
+        }
+
+        .buttonStyle(.plain)
+
+        .glass()
+
+    }
+
+}
