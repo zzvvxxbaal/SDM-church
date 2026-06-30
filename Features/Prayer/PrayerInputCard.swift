7b165d37b6b24c4c37602e13444b97efbc3c5fed
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 19:24:16 +0900

    PrayerInputCard.swift 업데이트

--- Features/Prayer/PrayerInputCard.swift
+++ Features/Prayer/PrayerInputCard.swift
@@ -1,1 +1,43 @@
+import SwiftUI
 
+struct PrayerInputCard: View {
+
+    @State
+
+    private var prayer = ""
+
+    var body: some View {
+
+        LiquidCard {
+
+            Text("기도제목 작성")
+
+                .font(.headline)
+
+            TextEditor(
+
+                text: $prayer
+
+            )
+
+            .frame(height: 130)
+
+            .scrollContentBackground(.hidden)
+
+            .background(Color.clear)
+
+            Button {
+
+            } label: {
+
+                Text("등록")
+
+            }
+
+            .buttonStyle(LiquidButton())
+
+        }
+
+    }
+
+}
