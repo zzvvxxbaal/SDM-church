acf216fe84fbe4d4dc96f3b48127c3125b45db5d
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 19:16:39 +0900

    AttendanceCard.swift 업데이트

--- Features/Home/AttendanceCard.swift
+++ Features/Home/AttendanceCard.swift
@@ -1,1 +1,47 @@
+import SwiftUI
 
+struct AttendanceCard: View {
+
+    var body: some View {
+
+        LiquidCard {
+
+            HStack {
+
+                VStack(alignment: .leading, spacing: 8) {
+
+                    Text("이번 달 출석")
+
+                        .font(.headline)
+
+                    Text("4 / 5")
+
+                        .font(.system(size: 34, weight: .bold))
+
+                }
+
+                Spacer()
+
+                VStack(alignment: .trailing, spacing: 8) {
+
+                    Text("80%")
+
+                        .font(.title3.bold())
+
+                    LiquidProgress(
+
+                        value: 0.8
+
+                    )
+
+                    .frame(width: 120)
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
