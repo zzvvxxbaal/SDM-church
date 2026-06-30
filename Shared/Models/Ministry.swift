305bafdcedc4aeaeff448db3f6e737c75dcff05f
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 19:34:54 +0900

    Ministry.swift 업데이트

--- Shared/Models/Ministry.swift
+++ Shared/Models/Ministry.swift
@@ -1,1 +1,15 @@
- 
+import Foundation
+
+struct Ministry: Identifiable, Hashable {
+
+    let id = UUID()
+
+    let title: String
+
+    let leader: String
+
+    let meeting: String
+
+    let description: String
+
+}
