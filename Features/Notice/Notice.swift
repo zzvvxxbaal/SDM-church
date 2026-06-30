c09cab5a4bd0a5f0343bd7668a0a6abef3b5faa4
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 18:30:15 +0900

    Notice.swift 업데이트

--- Features/Notice/Notice.swift
+++ Features/Notice/Notice.swift
@@ -1,1 +1,15 @@
+import Foundation
 
+struct Notice: Identifiable, Hashable {
+
+    let id = UUID()
+
+    let title: String
+
+    let summary: String
+
+    let date: String
+
+    let isPinned: Bool
+
+}
