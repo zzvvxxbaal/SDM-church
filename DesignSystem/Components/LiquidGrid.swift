d6833e436089182a1f9442106b6eef54080abba8
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 19:08:42 +0900

    LiquidGrid.swift 업데이트

--- DesignSystem/Components/LiquidGrid.swift
+++ DesignSystem/Components/LiquidGrid.swift
@@ -1,1 +1,33 @@
+import SwiftUI
 
+struct LiquidGrid<Content: View>: View {
+
+    @ViewBuilder
+
+    let content: Content
+
+    private let columns = [
+
+        GridItem(.flexible()),
+
+        GridItem(.flexible())
+
+    ]
+
+    var body: some View {
+
+        LazyVGrid(
+
+            columns: columns,
+
+            spacing: 18
+
+        ) {
+
+            content
+
+        }
+
+    }
+
+}
