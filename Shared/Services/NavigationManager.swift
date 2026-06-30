d34f06862de36f7b0138917d961b0d28e3ef2287
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 19:05:41 +0900

    NavigationManager.swift 업데이트

--- Shared/Services/NavigationManager.swift
+++ Shared/Services/NavigationManager.swift
@@ -1,1 +1,42 @@
+import SwiftUI
+import Observation
 
+@Observable
+final class NavigationManager {
+
+    enum Destination: Hashable {
+
+        case home
+        case notice
+        case worship
+        case prayer
+        case calendar
+        case community
+        case profile
+        case settings
+
+    }
+
+    var path = NavigationPath()
+
+    func push(_ destination: Destination) {
+
+        path.append(destination)
+
+    }
+
+    func pop() {
+
+        guard !path.isEmpty else { return }
+
+        path.removeLast()
+
+    }
+
+    func popToRoot() {
+
+        path = NavigationPath()
+
+    }
+
+}
