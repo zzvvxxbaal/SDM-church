8f798229c7455a0b76754dc0f3e0eddb1c3913fb
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 19:36:19 +0900

    Devotional.swift 업데이트

--- Shared/Models/Devotional.swift
+++ Shared/Models/Devotional.swift
@@ -1,1 +1,15 @@
+import Foundation
 
+struct Devotional: Identifiable {
+
+    let id = UUID()
+
+    let title: String
+
+    let date: Date
+
+    let scripture: String
+
+    let summary: String
+
+}
