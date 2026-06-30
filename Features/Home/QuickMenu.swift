ae17df4ca8c4d080a6c8de1ee729e537fc19ef31
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 18:57:18 +0900

    QuickMenu.swift 업데이트

--- Features/Home/QuickMenu.swift
+++ Features/Home/QuickMenu.swift
@@ -1,1 +1,91 @@
+import SwiftUI
 
+struct QuickMenu: View {
+
+    var body: some View {
+
+        HStack {
+
+            menu(
+
+                "book.fill",
+
+                "말씀"
+
+            )
+
+            Spacer()
+
+            menu(
+
+                "calendar",
+
+                "일정"
+
+            )
+
+            Spacer()
+
+            menu(
+
+                "hands.sparkles.fill",
+
+                "기도"
+
+            )
+
+            Spacer()
+
+            menu(
+
+                "person.3.fill",
+
+                "모임"
+
+            )
+
+        }
+
+    }
+
+    @ViewBuilder
+
+    private func menu(
+
+        _ image: String,
+
+        _ title: String
+
+    ) -> some View {
+
+        VStack(spacing: 12) {
+
+            ZStack {
+
+                Circle()
+
+                    .frame(
+
+                        width: 62,
+
+                        height: 62
+
+                    )
+
+                    .glass()
+
+                Image(systemName: image)
+
+                    .font(.title3)
+
+            }
+
+            Text(title)
+
+                .font(.caption)
+
+        }
+
+    }
+
+}
