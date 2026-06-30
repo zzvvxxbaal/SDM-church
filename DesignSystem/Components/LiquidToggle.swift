08bbad4a4fb46aa3ff37a6557a05ce123228198d
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 19:01:10 +0900

    LiquidToggle.swift 업데이트

--- DesignSystem/Components/LiquidToggle.swift
+++ DesignSystem/Components/LiquidToggle.swift
@@ -1,1 +1,61 @@
+import SwiftUI
 
+struct LiquidToggle: ToggleStyle {
+
+    func makeBody(configuration: Configuration) -> some View {
+
+        HStack {
+
+            configuration.label
+
+            Spacer()
+
+            RoundedRectangle(
+
+                cornerRadius: 18
+
+            )
+
+            .fill(
+
+                configuration.isOn
+
+                ? AppColors.tint
+
+                : .gray.opacity(0.25)
+
+            )
+
+            .frame(
+
+                width: 54,
+
+                height: 32
+
+            )
+
+            .overlay(alignment: configuration.isOn ? .trailing : .leading) {
+
+                Circle()
+
+                    .fill(.white)
+
+                    .padding(3)
+
+            }
+
+            .onTapGesture {
+
+                withAnimation(.snappy) {
+
+                    configuration.isOn.toggle()
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
