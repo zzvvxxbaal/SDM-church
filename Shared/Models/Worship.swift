294e35e867a3c0e885f888b041b11b8955f4c906
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 18:42:18 +0900

    Worship.swift 업데이트

--- Shared/Models/Worship.swift
+++ Shared/Models/Worship.swift
@@ -1,1 +1,17 @@
+import Foundation
 
+struct Worship: Identifiable {
+
+    let id = UUID()
+
+    let title: String
+
+    let date: Date
+
+    let location: String
+
+    let speaker: String
+
+    let verse: String
+
+}
