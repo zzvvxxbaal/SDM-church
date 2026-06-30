be68e337fd6ab735304c569141902fa8e0ad7238
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 19:32:57 +0900

    NoticeHeader.swift 업데이트

--- Features/Notice/NoticeHeader.swift
+++ Features/Notice/NoticeHeader.swift
@@ -1,1 +1,57 @@
+import SwiftUI
 
+struct NoticeHeader: View {
+
+    var body: some View {
+
+        ScrollView(.horizontal, showsIndicators: false) {
+
+            HStack(spacing: 12) {
+
+                NoticeCategoryChip(
+
+                    title: "전체",
+
+                    selected: true
+
+                )
+
+                NoticeCategoryChip(
+
+                    title: "공지",
+
+                    selected: false
+
+                )
+
+                NoticeCategoryChip(
+
+                    title: "행사",
+
+                    selected: false
+
+                )
+
+                NoticeCategoryChip(
+
+                    title: "예배",
+
+                    selected: false
+
+                )
+
+                NoticeCategoryChip(
+
+                    title: "소그룹",
+
+                    selected: false
+
+                )
+
+            }
+
+        }
+
+    }
+
+}
