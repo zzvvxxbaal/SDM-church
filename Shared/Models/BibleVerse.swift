ada38288c2777fd7fde77d10cdd0471a6d003286
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 19:14:09 +0900

    BibleVerse.swift 업데이트

--- Shared/Models/BibleVerse.swift
+++ Shared/Models/BibleVerse.swift
@@ -1,1 +1,15 @@
+import Foundation
 
+struct BibleVerse: Identifiable, Hashable {
+
+    let id = UUID()
+
+    let book: String
+
+    let chapter: Int
+
+    let verse: String
+
+    let content: String
+
+}
