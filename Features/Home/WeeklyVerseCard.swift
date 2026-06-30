b5bd51088cbf50d9359e94d618c4da80f72e3468
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 19:28:05 +0900

    WeeklyVerseCard.swift 업데이트

--- Features/Home/WeeklyVerseCard.swift
+++ Features/Home/WeeklyVerseCard.swift
@@ -1,1 +1,37 @@
+import SwiftUI
 
+struct WeeklyVerseCard: View {
+
+    var body: some View {
+
+        LiquidCard {
+
+            VStack(alignment: .leading, spacing: 16) {
+
+                Label(
+
+                    "이번 주 말씀",
+
+                    systemImage: "book.closed.fill"
+
+                )
+                .font(.headline)
+
+                Text("""
+믿음은 바라는 것들의 실상이요
+보이지 않는 것들의 증거니
+""")
+
+                .font(.title3.weight(.medium))
+
+                Text("히브리서 11:1")
+
+                    .foregroundStyle(.secondary)
+
+            }
+
+        }
+
+    }
+
+}
