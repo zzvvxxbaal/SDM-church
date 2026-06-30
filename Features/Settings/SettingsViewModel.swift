8619618a959b57f7b4ca2a366db2e923c0977e2a
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 19:12:32 +0900

    SettingsViewModel.swift 업데이트

--- Features/Settings/SettingsViewModel.swift
+++ Features/Settings/SettingsViewModel.swift
@@ -1,20 +1,50 @@
-import Foundation
 import Observation
 
 @Observable
+
 final class SettingsViewModel {
 
     let items = [
 
-        "알림",
+        SettingItem(
 
-        "다크모드",
+            title: "알림",
 
-        "개인정보",
+            icon: "bell.badge"
 
-        "문의하기",
+        ),
 
-        "앱 정보"
+        SettingItem(
+
+            title: "다크 모드",
+
+            icon: "moon"
+
+        ),
+
+        SettingItem(
+
+            title: "개인정보",
+
+            icon: "lock"
+
+        ),
+
+        SettingItem(
+
+            title: "문의하기",
+
+            icon: "bubble.left"
+
+        ),
+
+        SettingItem(
+
+            title: "앱 정보",
+
+            icon: "info.circle"
+
+        )
 
     ]
 
