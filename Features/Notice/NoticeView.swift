615e5ebd1e1c6f9ff20b4b3e2307cc81f4bdd793
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 18:32:55 +0900

    NoticeView.swift 업데이트

--- Features/Notice/NoticeView.swift
+++ Features/Notice/NoticeView.swift
@@ -1,1 +1,63 @@
+import SwiftUI
 
+struct NoticeView: View {
+
+    @State
+
+    private var viewModel = NoticeViewModel()
+
+    @State
+
+    private var search = ""
+
+    var body: some View {
+
+        ZStack {
+
+            MeshGradientBackground()
+
+            ScrollView {
+
+                VStack(spacing: 24) {
+
+                    LiquidLargeTitle(
+
+                        title: "공지사항",
+
+                        subtitle: "청년부 소식"
+
+                    )
+
+                    LiquidSearchBar(
+
+                        text: $search
+
+                    )
+
+                    ForEach(viewModel.notices) {
+
+                        NoticeCard(
+
+                            notice: $0
+
+                        )
+
+                    }
+
+                }
+
+                .padding(24)
+
+            }
+
+        }
+
+    }
+
+}
+
+#Preview {
+
+    NoticeView()
+
+}
