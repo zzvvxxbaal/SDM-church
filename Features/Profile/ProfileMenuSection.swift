6498d08a6722688459fbfab34262aa2ab1fd0999
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 19:10:50 +0900

    ProfileMenuSection.swift 업데이트

--- Features/Profile/ProfileMenuSection.swift
+++ Features/Profile/ProfileMenuSection.swift
@@ -1,1 +1,49 @@
+import SwiftUI
 
+struct ProfileMenuSection: View {
+
+    var body: some View {
+
+        VStack(spacing: 14) {
+
+            LiquidListRow {
+
+                Label(
+
+                    "내 정보",
+
+                    systemImage: "person.crop.circle"
+
+                )
+
+            }
+
+            LiquidListRow {
+
+                Label(
+
+                    "기도제목",
+
+                    systemImage: "hands.sparkles"
+
+                )
+
+            }
+
+            LiquidListRow {
+
+                Label(
+
+                    "설정",
+
+                    systemImage: "gearshape"
+
+                )
+
+            }
+
+        }
+
+    }
+
+}
