5ee68c766d955895e690ef1540ae995750b272d3
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 19:02:40 +0900

    LiquidLoadingView.swift 업데이트

--- DesignSystem/Components/LiquidLoadingView.swift
+++ DesignSystem/Components/LiquidLoadingView.swift
@@ -1,1 +1,71 @@
+import SwiftUI
 
+struct LiquidLoadingView: View {
+
+    @State
+
+    private var rotate = 0.0
+
+    var body: some View {
+
+        Circle()
+
+            .trim(
+
+                from: 0,
+
+                to: 0.75
+
+            )
+
+            .stroke(
+
+                AppColors.tint,
+
+                style: StrokeStyle(
+
+                    lineWidth: 5,
+
+                    lineCap: .round
+
+                )
+
+            )
+
+            .frame(
+
+                width: 40,
+
+                height: 40
+
+            )
+
+            .rotationEffect(
+
+                .degrees(
+
+                    rotate
+
+                )
+
+            )
+
+            .onAppear {
+
+                withAnimation(
+
+                    .linear(duration: 1)
+
+                    .repeatForever(autoreverses: false)
+
+                ) {
+
+                    rotate = 360
+
+                }
+
+            }
+
+    }
+
+}
