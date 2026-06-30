9dafa3f0d31c700ba4c81167f22d4bb996325b68
zzvvxxbaal <w1tns61@gmail.com>
2026-06-30 18:55:11 +0900

    DateFormatter+.swift 업데이트

--- Shared/Utilities/DateFormatter+.swift
+++ Shared/Utilities/DateFormatter+.swift
@@ -1,1 +1,17 @@
+import Foundation
 
+extension Date {
+
+    var koreanDate: String {
+
+        let formatter = DateFormatter()
+
+        formatter.locale = Locale(identifier: "ko_KR")
+
+        formatter.dateStyle = .long
+
+        return formatter.string(from: self)
+
+    }
+
+}
