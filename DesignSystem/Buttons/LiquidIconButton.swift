48beb1d5b16a01be75e04631e0eea3d905f82b48
zzvvxxbaal <w1tns61@gmail.com>
2026-07-02 19:12:11 +0900

    LiquidIconButton.swift 업데이트

--- DesignSystem/Buttons/LiquidIconButton.swift
+++ DesignSystem/Buttons/LiquidIconButton.swift
@@ -1,1 +1,28 @@
+var body: some View {
 
+    Button(
+        action: action
+    ) {
+
+        Image(
+            systemName: systemImage
+        )
+
+        .font(
+            .title3.weight(.semibold)
+        )
+
+        .frame(
+            width: 48,
+            height: 48
+        )
+
+    }
+
+    .buttonStyle(
+        PlainButtonStyle()
+    )
+
+    .glass(.button)
+
+}
