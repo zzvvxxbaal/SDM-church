7fbee3fc882ff05c118e1ff6018ca829e784fc57
zzvvxxbaal <w1tns61@gmail.com>
2026-07-02 19:10:39 +0900

    GlassModifier.swift 업데이트

--- Core/Glass/GlassModifier.swift
+++ Core/Glass/GlassModifier.swift
@@ -1,1 +1,37 @@
+import SwiftUI
 
+struct GlassModifier: ViewModifier {
+
+    var material: GlassProvider.Material = .regular
+
+    func body(content: Content) -> some View {
+
+        GlassSurface(
+            material: material
+        ) {
+
+            content
+
+        }
+
+    }
+
+}
+
+extension View {
+
+    func glass(
+        _ material: GlassProvider.Material = .regular
+    ) -> some View {
+
+        modifier(
+
+            GlassModifier(
+                material: material
+            )
+
+        )
+
+    }
+
+}
