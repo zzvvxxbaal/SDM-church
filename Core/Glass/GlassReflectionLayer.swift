aca64d92ce40a6a7d25ed23312e73df242d6cd41
zzvvxxbaal <w1tns61@gmail.com>
2026-07-02 19:09:45 +0900

    GlassReflectionLayer.swift 업데이트

--- Core/Glass/GlassReflectionLayer.swift
+++ Core/Glass/GlassReflectionLayer.swift
@@ -24,7 +24,7 @@ struct GlassReflectionLayer: View {
 
                     Color.white.opacity(opacity),
 
-                    Color.white.opacity(opacity * 0.25),
+                    Color.white.opacity(opacity * 0.35),
 
                     .clear
 
